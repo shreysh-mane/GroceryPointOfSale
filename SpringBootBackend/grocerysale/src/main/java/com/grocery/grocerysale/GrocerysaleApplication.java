25c7ebc7c936a8b5bcea2725d0c718b853ff4db1
shreysh-mane <shreysh.mane12@gmail.com>
2022-04-07 12:34:21 +0530

    firstCommit

---
+++ SpringBootBackend/grocerysale/src/main/java/com/grocery/grocerysale/GrocerysaleApplication.java
@@ -0,0 +1,14 @@
+package com.grocery.grocerysale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocerysaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrocerysaleApplication.class, args);
+		System.out.println("application started");
+	}
+
+}
---
+++ SpringBootBackend/grocerysale/src/test/java/com/grocery/grocerysale/GrocerysaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grocery.grocerysale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocerysaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
